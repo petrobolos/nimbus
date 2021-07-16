2e23cdb993c2d8732dc240237d1e75adfb95348c
Oliver Earl <oliver@oliverearl.co.uk>
2021-07-17 00:35:26 +0100

    Fresh installation of Laravel.

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+/**
+ * Class DatabaseSeeder
+ *
+ * @package Database\Seeders
+ */
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        User::factory(10)->create();
+    }
+}
