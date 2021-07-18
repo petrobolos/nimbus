9fe1c7f1a81b1fa5740720d8b61976222638c732
Oliver Earl <oliver@oliverearl.co.uk>
2021-07-18 17:49:37 +0100

    Change test style.

--- tests/Unit/Models/UserTest.php
+++ tests/Unit/Models/UserTest.php
@@ -12,8 +12,7 @@ use Tests\TestCaseWithDatabase;
  */
 final class UserTest extends TestCaseWithDatabase
 {
-    /** @test */
-    public function is_admin_will_return_true_if_the_given_user_is_an_administrator(): void
+    public function test_is_admin_will_return_true_if_the_given_user_is_an_administrator(): void
     {
         /** @var \App\Models\User $admin */
         $admin = User::factory()->admin()->create();
@@ -21,8 +20,7 @@ final class UserTest extends TestCaseWithDatabase
         self::assertTrue($admin->isAdmin());
     }
 
-    /** @test */
-    public function is_admin_will_return_false_if_the_given_user_is_not_an_administrator(): void
+    public function test_is_admin_will_return_false_if_the_given_user_is_not_an_administrator(): void
     {
         /** @var \App\Models\User $notAnAdmin */
         $notAnAdmin = User::factory()->notAnAdmin()->create();
