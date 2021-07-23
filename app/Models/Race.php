5c4602d1fab8e94f7d2c52e2c9c4c75ca7394d1f
Oliver Earl <oliver@oliverearl.co.uk>
2021-07-23 22:49:57 +0100

    Add races and casts.

--- app/Models/Race.php
+++ app/Models/Race.php
@@ -15,26 +15,55 @@ class Race extends Model
 {
     use HasFactory;
 
+    public const IMPORT_SHEET = 'game/Races.xlsx';
+
     public const RACE_HUMAN = 'human';
     public const RACE_SAIYAN = 'saiyan';
+    public const RACE_SUPER_SAIYAN = 'super_saiyan';
     public const RACE_SAIYAN_GOD = 'saiyan_god';
+    public const RACE_SUPER_SAIYAN_GOD = 'super_saiyan_god';
     public const RACE_NAMEKIAN = 'namek';
     public const RACE_ANDROID = 'android';
     public const RACE_DEMON = 'demon';
     public const RACE_MAJIN = 'majin';
     public const RACE_FRIEZA = 'frieza';
     public const RACE_GOD_OF_DESTRUCTION = 'god_of_destruction';
+    public const ROBOT = 'robot';
 
     public const RACES = [
         self::RACE_HUMAN,
         self::RACE_SAIYAN,
+        self::RACE_SUPER_SAIYAN,
         self::RACE_SAIYAN_GOD,
+        self::RACE_SUPER_SAIYAN_GOD,
         self::RACE_NAMEKIAN,
         self::RACE_ANDROID,
         self::RACE_DEMON,
         self::RACE_MAJIN,
         self::RACE_FRIEZA,
         self::RACE_GOD_OF_DESTRUCTION,
+        self::ROBOT,
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'code',
+        'description',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
     ];
 
     /**
---
+++ database/migrations/2021_07_23_205444_add_s_p_to_fighters.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('fighters', function (Blueprint $table) {
+            $table->unsignedBigInteger('sp')->default(0)->after('hp');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('fighters', function (Blueprint $table) {
+            $table->dropColumn('sp');
+        });
+    }
+};
