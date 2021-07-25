bf7735c52512178835d645334159e4f5d00f30e5
Oliver Earl <oliver@oliverearl.co.uk>
2021-07-25 13:53:18 +0100

    Honestly, Breeze and Laravel view components in general are a fucking overcomplicated mess.
    Ripping Breeze out and going back to laravel/ui

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,7 @@ Route::middleware(['auth'])->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
