dc32ee893e3e729f740c1979627c2e81293cb287
Oliver Earl <oliver@oliverearl.co.uk>
2021-07-25 23:22:59 +0100

    Install TypeScript

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -14,10 +14,14 @@
     </div>
 </template>
 
-<script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component
+export default class ExampleComponent extends Vue {
+    mounted(): void {
+        console.log('Hello world!!');
     }
+};
 </script>
