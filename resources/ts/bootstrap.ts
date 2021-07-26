3b8a1db371ddd6407c87d6a8aa48c81e5b5cdbab
Oliver Earl <oliver@oliverearl.co.uk>
2021-07-26 21:56:18 +0100

    Install Echo/Pusher.

--- resources/ts/bootstrap.ts
+++ resources/ts/bootstrap.ts
@@ -3,12 +3,25 @@ import * as _ from 'lodash';
 import jQuery from 'jquery';
 import * as Popper from 'popper.js';
 import 'bootstrap';
+import Echo from "laravel-echo";
+import Pusher from 'pusher-js';
 
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-let token : HTMLMetaElement | null = document.head!.querySelector('meta[name="csrf-token"]');
+let token: HTMLMetaElement | null = document.head!.querySelector('meta[name="csrf-token"]');
+
 if (token) {
     axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 } else {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
+
+(<any>window).Pusher = Pusher;
+
+(<any>window).Echo = new Echo({
+    broadcaster: 'pusher',
+    key: process.env.MIX_PUSHER_APP_KEY,
+    wsHost: process.env.MIX_PUSHER_HOST,
+    wsPort: process.env.MIX_PUSHER_PORT,
+    forceTLS: process.env.APP_ENV = 'local' ? 'false' : 'true',
+});
