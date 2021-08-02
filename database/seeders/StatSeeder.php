4813d815ada23f72a0fa3948b73f361e8bb57518
Oliver Earl <oliver@oliverearl.co.uk>
2021-08-02 23:46:48 +0100

    Add seeder.

--- database/seeders/StatSeeder.php
+++ database/seeders/StatSeeder.php
@@ -2,8 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Models\Stat;
 use Illuminate\Database\Seeder;
 
+/**
+ * Class StatSeeder
+ *
+ * @package Database\Seeders
+ */
 class StatSeeder extends Seeder
 {
     /**
@@ -13,6 +19,12 @@ class StatSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Stat::factory()->bronze()->create();
+        Stat::factory()->silver()->create();
+        Stat::factory()->gold()->create();
+        Stat::factory()->platinum()->create();
+        Stat::factory()->diamond()->create();
+        Stat::factory()->master()->create();
+        Stat::factory()->grandmaster()->create();
     }
 }
