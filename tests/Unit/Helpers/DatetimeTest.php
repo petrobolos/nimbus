63ec3b8e3e9e0450ce1042d5fe4a7ecd36104b82
Oliver Earl <oliver@oliverearl.co.uk>
2021-08-14 18:03:14 +0100

    Fix broken test.

--- tests/Unit/Helpers/DatetimeTest.php
+++ tests/Unit/Helpers/DatetimeTest.php
@@ -10,7 +10,7 @@ final class DatetimeTest extends TestCase
     public function test_human_readable_datetime_returns_a_formatted_string(): void
     {
         $datetime = Carbon::parse('2021-08-14 13:27:28');
-        $formattedString = 'August 14 2021, 1:27pm';
+        $formattedString = 'August 14th 2021, 1:27pm';
 
         self::assertEquals($formattedString, humanReadableDatetime($datetime));
     }
