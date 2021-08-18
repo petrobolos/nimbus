1b65c7cb9f3ea9b88448a80b86d7cb45a7c05fdc
Oliver Earl <oliver@oliverearl.co.uk>
2021-08-19 00:57:18 +0100

    Add meta tags to heading.

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -1,21 +1,26 @@
 <head>
     @include('layouts.partials.comment')
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="author" content="Petrobolos Games" />
+    <meta name="description" content="@yield('meta-description')" />
+    <meta name="robots" content="@yield('meta-robots')" />
+
+    {{-- TODO: Add favicon and social media images, icons, etc. --}}
 
     <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-    <title>@yield('title') | {{ config('app.name', 'DBGT.net') }}</title>
+    <title>@yield('title') | {{ config('app.name', 'Nimbus') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="dns-prefetch" href="//fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" />
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
 </head>
--- resources/views/pages/game/game.blade.php
+++ resources/views/pages/game/game.blade.php
@@ -2,6 +2,11 @@
 
 @section('title', "Game {$game->id}")
 
+@section('meta-robots', 'noindex,nofollow')
+
+{{-- TODO: Insert game description --}}
+@section('meta-description', 'Description')
+
 @section('content')
     <div class="container">
         <game-component :initial-game="{{ json_encode($game, JSON_THROW_ON_ERROR) }}"></game-component>
