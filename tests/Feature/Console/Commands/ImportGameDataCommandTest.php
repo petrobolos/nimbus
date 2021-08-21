9be9a04aaf1f437c1d39703c54e10578b767be14
Oliver Earl <oliver@oliverearl.co.uk>
2021-08-22 00:09:21 +0100

    Expand test suite to test error handling.

--- tests/Feature/Console/Commands/ImportGameDataCommandTest.php
+++ tests/Feature/Console/Commands/ImportGameDataCommandTest.php
@@ -2,8 +2,10 @@
 
 namespace Tests\Feature\Console\Commands;
 
+use Illuminate\Support\Facades\Config;
 use Maatwebsite\Excel\Facades\Excel;
 use Tests\TestCaseWithDatabase;
+use Throwable;
 
 /**
  * Class ImportGameDataCommandTest
@@ -19,5 +21,23 @@ final class ImportGameDataCommandTest extends TestCaseWithDatabase
         $this->artisan('import:game')
             ->expectsOutput('Starting import!')
             ->assertExitCode(0);
+    }
+
+    public function test_import_game_data_command_will_throw_a_validation_exception_because_of_broken_data(): void
+    {
+        $this->expectException(Throwable::class);
+
+        Config::set('game.imports.dir', base_path('tests/Fixtures/Imports/'));
+
+        $this->artisan('import:game');
+    }
+
+    public function test_import_game_data_import_throws_an_exception_as_file_is_completely_missing(): void
+    {
+        $this->expectException(Throwable::class);
+
+        Config::set('game.imports.dir', base_path());
+
+        $this->artisan('import:game');
     }
 }
