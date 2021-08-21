ba226f500ee07cad4e4364d74517b6ed87042124
Oliver Earl <oliver@oliverearl.co.uk>
2021-08-21 22:30:47 +0100

    Additional test to check exception behaviour.

--- tests/Unit/Helpers/FilesystemTest.php
+++ tests/Unit/Helpers/FilesystemTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit\Helpers;
 
+use App\Exceptions\CopyrightNoticeMissingException;
 use Tests\TestCase;
 
 /**
@@ -21,5 +22,30 @@ final class FilesystemTest extends TestCase
         $copyrightText = copyrightNotice();
 
         self::assertStringContainsStringIgnoringCase($expectedNeedle, $copyrightText);
+    }
+
+    /** @throws \Psr\SimpleCache\InvalidArgumentException */
+    public function test_copyright_notice_throws_an_exception_if_the_file_is_missing(): void
+    {
+        $copyrightFile = base_path('COPYRIGHT');
+
+        if (! file_exists($copyrightFile)) {
+            $this->markTestSkipped('Copyright file is missing - cannot perform this test...');
+        }
+
+        // Delete copyright file.
+        $fileContents = file_get_contents($copyrightFile);
+        unlink($copyrightFile);
+
+        try {
+            copyrightNotice();
+            $this->fail('No exception was thrown...');
+        } catch (CopyrightNoticeMissingException $exception) {
+            self::assertInstanceOf(CopyrightNoticeMissingException::class, $exception);
+        }
+
+        // Recreate the file.
+        clearstatcache();
+        file_put_contents(base_path('COPYRIGHT'), $fileContents);
     }
 }
