be7891361627897799e2cd5937342f52fc2c5480
Oliver Earl <oliver@oliverearl.co.uk>
2021-08-21 12:49:05 +0100

    Separate props into separate Blade files.

--- resources/views/pages/game/demo.blade.php
+++ resources/views/pages/game/demo.blade.php
@@ -1,1 +1,10 @@
 @extends('pages.game.game')
+
+@section('content')
+    <div class="container">
+        <game-component
+            :initial-game="{{ json_encode($game, JSON_THROW_ON_ERROR) }}"
+            :demo-information="{{ json_encode($demo, JSON_THROW_ON_ERROR) }}"
+        ></game-component>
+    </div>
+@endsection
--- resources/views/pages/game/game.blade.php
+++ resources/views/pages/game/game.blade.php
@@ -8,9 +8,7 @@
 
 @section('content')
     <div class="container">
-        <game-component
-            :initial-game="{{ json_encode($game, JSON_THROW_ON_ERROR) }}"
-            :demo-information="{{ json_encode($demo, JSON_THROW_ON_ERROR) }}">
+        <game-component :initial-game="{{ json_encode($game, JSON_THROW_ON_ERROR) }}">
         </game-component>
     </div>
 @endsection
