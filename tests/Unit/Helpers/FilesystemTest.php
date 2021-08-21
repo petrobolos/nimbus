7e4472eaf5268eab1aa7992b9c12deb156ad1af7
Oliver Earl <oliver@oliverearl.co.uk>
2021-08-22 00:16:32 +0100

    Improve coverage to test the caching system also.

--- tests/Unit/Helpers/FilesystemTest.php
+++ tests/Unit/Helpers/FilesystemTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Helpers;
 
 use App\Exceptions\CopyrightNoticeMissingException;
+use Illuminate\Support\Facades\Cache;
 use Tests\TestCase;
 
 /**
@@ -12,16 +13,17 @@ use Tests\TestCase;
  */
 final class FilesystemTest extends TestCase
 {
+    public const COPYRIGHT_NEEDLE = 'The game will not operate without this copyright message present';
+
     /**
      * @throws \App\Exceptions\CopyrightNoticeMissingException
      * @throws \Psr\SimpleCache\InvalidArgumentException
      */
     public function test_copyright_notice_returns_the_correct_message(): void
     {
-        $expectedNeedle = 'The game will not operate without this copyright message present';
         $copyrightText = copyrightNotice();
 
-        self::assertStringContainsStringIgnoringCase($expectedNeedle, $copyrightText);
+        self::assertStringContainsStringIgnoringCase(self::COPYRIGHT_NEEDLE, $copyrightText);
     }
 
     /** @throws \Psr\SimpleCache\InvalidArgumentException */
@@ -47,5 +49,27 @@ final class FilesystemTest extends TestCase
         // Recreate the file.
         clearstatcache();
         file_put_contents(base_path('COPYRIGHT'), $fileContents);
+    }
+
+    /**
+     * @throws \App\Exceptions\CopyrightNoticeMissingException
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     */
+    public function test_copyright_notice_can_be_retrieved_from_cache_instead_of_loaded_from_file_on_each_request(): void
+    {
+        $key = 'COPYRIGHT';
+
+        Cache::shouldReceive('has')
+            ->once()
+            ->with($key)
+            ->andReturn(true);
+
+        Cache::shouldReceive('get')
+            ->once()
+            ->with($key)
+            ->andReturn(file_get_contents(base_path($key)));
+
+        $content = copyrightNotice();
+        self::assertStringContainsStringIgnoringCase(self::COPYRIGHT_NEEDLE, $content);
     }
 }
