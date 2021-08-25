416a15d6713a48cc271cab1cef7781cf4168dccf
Oliver Earl <oliver@oliverearl.co.uk>
2021-08-25 23:56:16 +0100

    Add frontend factory for fighters.

--- resources/ts/factories/ability.factory.ts
+++ resources/ts/factories/ability.factory.ts
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { Factory } from 'fishery';
 import * as Faker from 'faker';
+
 import AbilityInterface from '../interfaces/ability.interface';
 
 export default Factory.define<AbilityInterface>(({ sequence }) => ({
---
+++ resources/ts/factories/fighter.factory.ts
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { Factory } from 'fishery';
+import slugify from 'slugify';
+import * as Faker from 'faker';
+
+import FighterInterface from '../interfaces/fighter.interface';
+
+const name = Faker.company.companyName();
+export default Factory.define<FighterInterface>(({ sequence }) => ({
+  // TODO: Add in the ability to generate multiple abilities.
+  abilities: [],
+  attack: Faker.datatype.number(100),
+  code: slugify(name),
+  current_hp: 100,
+  current_sp: 100,
+  defense: Faker.datatype.number(100),
+  description: Faker.lorem.sentences(5),
+  hp: Faker.datatype.number(100),
+  sp: Faker.datatype.number(100),
+  id: sequence,
+  is_boss: Faker.datatype.boolean(),
+  is_paralyzed: Faker.datatype.boolean(),
+  name: name,
+  special: Faker.datatype.number(100),
+  speed: Faker.datatype.number(100),
+  spirit: Faker.datatype.number(100),
+  uuid: Faker.datatype.uuid(),
+}));
