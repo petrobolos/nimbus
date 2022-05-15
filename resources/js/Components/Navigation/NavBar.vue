e563ae071dca44d986399103fe6be1a63fe112fa
Oliver Earl <oliver.earl@petrobolos.com>
2022-05-15 19:04:34 +0100

    style: Adjust width.

--- resources/js/Components/Navigation/NavBar.vue
+++ resources/js/Components/Navigation/NavBar.vue
@@ -27,7 +27,7 @@ const userButtonClasses = computed(() => {
 <template>
     <nav class="bg-nimbus-primary">
         <!-- Navigation menu -->
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="mx-auto px-4 sm:px-6 lg:px-8">
             <div class="flex justify-between h-12">
                 <div class="flex">
                     <!-- Logo -->
