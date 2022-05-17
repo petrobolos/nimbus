a8378f2cde11c705df9d21a5de5071b3f8c8b59a
Oliver Earl <oliver.earl@petrobolos.com>
2022-05-17 19:28:42 +0100

    feat: Add new current party member ID to player.

---
+++ database/migrations/2022_05_17_190521_add_current_player_to_players.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    public function up(): void
+    {
+        Schema::table('players', function (Blueprint $table) {
+            $table
+                ->unsignedBigInteger('current_party_member_id')
+                ->after('party_member_id_3')
+                ->index();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('players', function (Blueprint $table) {
+            $table->dropColumn('current_party_member_id');
+        });
+    }
+};
