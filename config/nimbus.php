c53824ce6341bb7b535302a562ab36e505f90fc4
Oliver Earl <oliver.earl@petrobolos.com>
2022-05-18 19:18:48 +0100

    feat: Add universal abilities to config.

--- config/nimbus.php
+++ config/nimbus.php
@@ -1,6 +1,60 @@
 <?php
 
+use App\Enums\GameLogic\Abilities\AbilityType;
+
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Universal abilities
+    |--------------------------------------------------------------------------
+    |
+    | Any universal abilities that need to be made available to all playable fighters.
+    |
+    */
+    'universal_abilities' => [
+        [
+            'name' => 'Skip',
+            'cost' => 0,
+            'type' => AbilityType::SKIP,
+            'description' => 'Skips your turn without doing anything. Cures paralysis.',
+        ],
+        [
+            'name' => 'Attack',
+            'cost' => 0,
+            'type' => AbilityType::PHYSICAL,
+            'description' => "Standard physical attack. It doesn't cost anything.",
+        ],
+        [
+            'name' => 'Defend',
+            'cost' => 0,
+            'type' => AbilityType::BLOCK,
+            'description' => "Adopts a defensive pose to reduce incoming damage. It doesn't cost anything.",
+        ],
+        [
+            'name' => 'Heal',
+            'cost' => 20,
+            'type' => AbilityType::RECOVERY,
+            'description' => 'Restores HP and heals from injuries. Costs a large amount of SP.',
+        ],
+        [
+            'name' => 'Power Up',
+            'cost' => 0,
+            'type' => AbilityType::RECOVERY,
+            'description' => 'Restores SP but costs a turn.',
+        ],
+        [
+            'name' => 'Ki Blast',
+            'cost' => 2,
+            'type' => AbilityType::SPECIAL,
+            'description' => 'A basic energy blast.',
+        ],
+        [
+            'name' => 'Ki Wave',
+            'cost' => 4,
+            'type' => AbilityType::SPECIAL,
+            'description' => 'An energy blast directed as a continuous beam of ki.',
+        ],
+    ],
 
     /*
     |--------------------------------------------------------------------------
