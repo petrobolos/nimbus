cf40043666b1c4f21e97be9ffcc55c4c2a6d6a79
Oliver Earl <oliver.earl@petrobolos.com>
2022-05-21 11:03:33 +0100

    feat: Add descriptions and ability to convert from English to enum.

--- app/Enums/GameLogic/Abilities/AbilityType.php
+++ app/Enums/GameLogic/Abilities/AbilityType.php
@@ -2,6 +2,7 @@
 
 namespace App\Enums\GameLogic\Abilities;
 
+use Illuminate\Support\Str;
 use Petrobolos\FixedArray\FixedArray;
 use SplFixedArray;
 
@@ -14,6 +15,54 @@ enum AbilityType: int
     case RECOVERY = 3;
 
     /**
+     * A description of the skip type.
+     *
+     * @var string
+     */
+    public const SKIP_DESCRIPTION = 'skip';
+
+    /**
+     * A description of the block type.
+     *
+     * @var string
+     */
+    public const BLOCK_DESCRIPTION = 'block';
+
+    /**
+     * A description of the physical type.
+     *
+     * @var string
+     */
+    public const PHYSICAL_DESCRIPTION = 'physical';
+
+    /**
+     * A description of the special type.
+     *
+     * @var string
+     */
+    public const SPECIAL_DESCRIPTION = 'special';
+
+    /**
+     * A description of the recovery type.
+     *
+     * @var string
+     */
+    public const RECOVERY_DESCRIPTION = 'recovery';
+
+    /**
+     * An array of type descriptions.
+     *
+     * @var array
+     */
+    public const TYPE_DESCRIPTIONS = [
+        self::SKIP_DESCRIPTION,
+        self::BLOCK_DESCRIPTION,
+        self::PHYSICAL_DESCRIPTION,
+        self::SPECIAL_DESCRIPTION,
+        self::RECOVERY_DESCRIPTION,
+    ];
+
+    /**
      * Return the types of ability.
      *
      * @return \SplFixedArray
@@ -21,6 +70,24 @@ enum AbilityType: int
     public static function types(): SplFixedArray
     {
         return FixedArray::fromArray(self::cases());
+    }
+
+    /**
+     * Get the ability type value from a string interpretation.
+     *
+     * @param string $name
+     * @return int
+     */
+    public static function fromPretty(string $name): int
+    {
+        return match (Str::lower($name)) {
+            self::SKIP_DESCRIPTION => self::SKIP->value,
+            self::BLOCK_DESCRIPTION => self::BLOCK->value,
+            self::PHYSICAL_DESCRIPTION => self::PHYSICAL->value,
+            self::SPECIAL_DESCRIPTION => self::SPECIAL->value,
+            self::RECOVERY_DESCRIPTION => self::RECOVERY->value,
+            default => self::default(),
+        };
     }
 
     /**
