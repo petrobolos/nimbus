b0773fcd68a3743d723f5f798c61bf6a5045009a
Oliver Earl <oliver.earl@petrobolos.com>
2022-05-22 01:24:07 +0100

    feat: Add copyright protection in place.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use RuntimeException;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        /*
+         * We politely ask that you don't modify the copyright notices or its enforcement methods.
+         * You know what happened to DBZ:A.
+         */
+        if (! file_exists(base_path('COPYRIGHT'))) {
+            throw new RuntimeException('Nimbus will not operate without a copyright notice in place.');
+        }
+
+        define('COPY_PROTECT', 'c3a5967cbd0289fcb41b767a43896b37');
+        define('COPY_VERIFY', md5_file(base_path('COPYRIGHT')));
     }
 }
