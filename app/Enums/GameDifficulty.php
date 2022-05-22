65f7acf0e6086ca8675561c6b549b119f59b6fb0
Oliver Earl <oliver.earl@petrobolos.com>
2022-05-22 01:00:38 +0100

    refactor: No longer need this default method.

--- app/Enums/GameDifficulty.php
+++ app/Enums/GameDifficulty.php
@@ -7,14 +7,4 @@ enum GameDifficulty: string
     case EASY = 'easy';
     case NORMAL = 'normal';
     case HARD = 'hard';
-
-    /**
-     * Return the default game status.
-     *
-     * @return int
-     */
-    public static function default(): int
-    {
-        return self::EASY->value;
-    }
 }
